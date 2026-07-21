b5e372eb1dbe2a7d64c772a9b0869d73f12159c0
Rahmat Eka Satria <matsganz@gmail.com>
2026-07-21 16:34:24 +0700

    Bump web cache version

--- contracts/public/flutter_bootstrap.js
+++ contracts/public/flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"4c525dac5ebe5971c5708ef73558ed8edcf4a3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2155208256" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "taniin-v39" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
